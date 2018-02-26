9400591955e051f115c43196bc5e0986586e0ba7
Mareddy Venkateswara Reddy (Data Platform Engineering) <MA318100@wipro.com>
2018-02-26 12:13:58 +0530

    initial push

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var config = require('./config');//runs the config folder index.js and returns what it returns;
+var setupController = require('./controllers/setupController');
+
+
+var port = process.env.PORT || 3000;
+
+app.use('/assets',express.static(__dirname + '/public'));
+
+app.set('view-engine', 'ejs');
+
+mongoose.connect(config.getDbConnectionString());
+setupController(app);// seeding the database
+
+app.listen(port);
